648aacd2e191692803dd80b154a8430a0440ffb4
OpenKickStart <iiyoyuk@gmail.com>
2026-02-12 04:05:33 +0800

    Add recursive glob support with ** patterns

---
+++ glob_test.go
@@ -0,0 +1,20 @@
+package main
+
+import ("os";"path/filepath";"testing")
+
+func TestExpandSimple(t *testing.T) {
+	tmp := t.TempDir()
+	os.WriteFile(filepath.Join(tmp,"a.go"), []byte("x"), 0644)
+	ms := ExpandGlob(filepath.Join(tmp,"*.go"))
+	if len(ms) != 1 { t.Errorf("want 1, got %d", len(ms)) }
+}
+
+func TestExpandRecursive(t *testing.T) {
+	tmp := t.TempDir()
+	sub := filepath.Join(tmp,"sub")
+	os.MkdirAll(sub, 0755)
+	os.WriteFile(filepath.Join(tmp,"a.go"), []byte("x"), 0644)
+	os.WriteFile(filepath.Join(sub,"b.go"), []byte("y"), 0644)
+	ms := ExpandGlob(tmp+"/**/*.go")
+	if len(ms) != 2 { t.Errorf("want 2, got %d", len(ms)) }
+}
